78bc1fef2b51212a84672ef603f8f205239759e7
saka <reisuicupsoup@gmail.com>
2022-09-19 19:30:50 +0900

    Appに分割

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App = () => {
+    return (
+        <>
+            <h1 className="text-2xl">Hello!</h1>
+        </>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import "./tailwind.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { App } from "./App.tsx";
+
 const container = document.getElementById("root");
 const root = createRoot(container!);
-
-const App = () => <div>App</div>;
 
 root.render(<App />);
